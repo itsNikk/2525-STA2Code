e5f35e0e0821b9ba06b2ae5c2ec7cb94632d3a46
iTzNikk <nicolo.buganza@tiscali.it>
2026-03-08 21:30:34 +0100

    Init

---
+++ Playlist/playlist.py
@@ -0,0 +1,12 @@
+
+# Per aprire il terminale di VSCode usare la shortcut CTRL+J
+# oppure il menu Terminal in alto nell'editor.
+
+#1 Creare un file che abbia estensione .py
+#2 Scrivere codice!!
+#3 Nel terminale digitare python nomeDelTuoFile.py
+#4 vedere l'output del codice
+
+# In Python questo è un commento.
+# Un commento non fa parte del programma, sono delle note personali! :)
+print("Ciao")
