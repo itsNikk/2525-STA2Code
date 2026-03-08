2c9a7ccd18cd478312eb53f8e65dd1fa20b3f6b4
iTzNikk <nicolo.buganza@tiscali.it>
2026-03-08 21:43:46 +0100

    Update playlist.py

--- Playlist/playlist.py
+++ Playlist/playlist.py
@@ -1,4 +1,3 @@
-
 # Per aprire il terminale di VSCode usare la shortcut CTRL+J
 # oppure il menu Terminal in alto nell'editor.
 
@@ -8,5 +7,5 @@
 #4 vedere l'output del codice
 
 # In Python questo è un commento.
-# Un commento non fa parte del programma, sono delle note personali! :)
-print("Ciao")
+# I commenti sono solo note personali! :)
+print("Ciao");
