1f203ef5b540e23f9b1a017900dc9d210ffc0046
iTzNikk <nicolo.buganza@tiscali.it>
2026-03-09 15:48:29 +0100

    Update caffeine.py

---
+++ Caffeine-o-meter/caffeine.py
@@ -0,0 +1,5 @@
+nomeBevanda = input("Inserisci nome bevanda: ")
+mgCaffeina = int(input("mg caffeina per "+nomeBevanda+": "))
+
+print("La tua bevanda "+nomeBevanda+" ha " +
+      str(mgCaffeina) + " mg di caffeina")
